162b7e303e7f1cadbc2a5a12dbebb702a0a77188
DorianValentino <support@eliteshredoaching.com>
2025-11-24 18:42:26 +0100

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import React from "react";
-import "@/styles/mobile.css";
+import "./mobile.css";
 
 export const metadata: Metadata = {
   title: "EliteShred Dashboard",
