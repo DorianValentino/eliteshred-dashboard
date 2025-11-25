e363ebef2b377a5c1c6425c3b4a7b56f1174e893
DorianValentino <support@eliteshredoaching.com>
2025-11-25 11:12:59 +0100

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="de">
+      <head>
+        <meta
+         name="viewport"
+         content="width=device-width, inirial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+         />
+      </head>
       <body
         style={{
           margin: 0,
